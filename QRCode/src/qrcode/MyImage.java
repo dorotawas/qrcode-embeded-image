0bc3fcf6ddc29cb535bb528dd7695a87de5c2ad6
Dorota W?s <dorota.was@gmail.com>
2011-10-17 11:15:20 +0200

    Contrast

--- QRCode/src/qrcode/MyImage.java
+++ QRCode/src/qrcode/MyImage.java
@@ -29,6 +29,61 @@ public class MyImage {
         return image;
     }
 
+    public MyImage contrast() {
+
+        int W = image.getWidth();
+        int H = image.getHeight();
+        int[][] smallerTab = new int[W][H];
+        int[][] smallerTabNew = new int[W][H];
+        int[][][] colors = new int[W][H][3];
+        int maxColor = (1<<8)-1;
+        //for each pixel, check whether we should make it lighter (smaller > 0) or darker
+        for(int x=0; x<W; x++) {
+            for(int y=0; y<H; y++) {
+                int mid = maxColor/2;
+                int smaller = 0;
+                int RGB = image.getRGB(x, y);
+                for(int i=0; i<3; i++) {
+                    colors[x][y][i] = RGB & maxColor;
+                    if(colors[x][y][i] < mid)
+                        smaller++;
+                    else
+                        smaller--;
+                    RGB = RGB >> 8;
+                }
+                smallerTab[x][y] = smaller;
+            }
+        }
+        // for each pixel, update smallerTable, depending on neighbours
+        for(int x=0; x<W; x++)
+            for(int y=0; y<H; y++) {
+                smallerTabNew[x][y] = 0;
+                for(int dx=-1; dx<=1; dx++)
+                    for(int dy=-1; dy<=1; dy++)
+                        if(x+dx >= 0 && x+dx<W && y+dy >= 0 && y+dy <H)
+                            smallerTabNew[x][y] += smallerTab[x+dx][y+dy];
+            }
+        smallerTab = smallerTabNew;
+        // for each pixel, make it darker or lighter
+        for(int x=0; x<W; x++) {
+            for(int y=0; y<H; y++) {
+                if(smallerTab[x][y] > 0) {
+                    for(int i=0; i<3; i++)
+                        colors[x][y][i] /=2;
+                }
+                else {
+                    for(int i=0; i<3; i++) {
+                        int dif = maxColor - colors[x][y][i];
+                        dif /= 2;
+                        colors[x][y][i] = maxColor - dif;
+                    }
+                }
+                image.setRGB(x, y, (colors[x][y][0]) | (colors[x][y][1]<<8) | (colors[x][y][2]<<16));
+            }
+        }
+        return this;
+    }
+
     public BufferedImage resizeTo(int width, int height) {
         BufferedImage resizedImage = new BufferedImage(width, height,
                 BufferedImage.TYPE_INT_ARGB);
